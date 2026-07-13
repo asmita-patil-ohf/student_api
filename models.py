27f8aa3f80bdb2c2cad30568939b09b263a23f62
asmita-patil-ohf <1646asmitapatil@gmail.com>
2026-07-13 22:02:14 +0530

    Added base, engine and session factory

--- models.py
+++ models.py
@@ -4,10 +4,9 @@ from database import Base
 
 class Student(Base):
     __tablename__ = "students"
-
-    id = Column(Integer, primary_key=True,  autoincrement=True)
-    name = Column(String(50))
-    roll_no = Column(Integer)
-    grade = Column(String(5))
-
-
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    roll_no = Column(Integer, nullable=False)
+    grade = Column(String(2), nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
