cc980c92998acd7cc11f6cf5535db02ee11c910f
asmita-patil-ohf <1646asmitapatil@gmail.com>
2026-08-05 23:45:28 +0530

    Updated security file

--- security.py
+++ security.py
@@ -1,1 +1,11 @@
-from passlib.context import CryptContext 
+from passlib.context import CryptContext
+
+# Create one password hashing object for the entire application
+pwd_context = CryptContext(
+    schemes=["bcrypt"],      # Use bcrypt hashing algorithm
+    deprecated="auto"        # Automatically handle deprecated algorithms
+)
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
